d91d28f563e8cdbb9abd30e430539da75c363576
Gennady Lyashenko <gena_l_1999@mail.ru>
2022-09-14 23:37:30 +0300

    [2] correction of comments

--- src/Overbeered.VkArchiver/IVkArchiver.cs
+++ src/Overbeered.VkArchiver/IVkArchiver.cs
@@ -7,7 +7,7 @@
     {
 
         /// <summary>
-        /// Сохраняет все фотографии из всех чатов/диалогов в зависимости от флага
+        /// Сохраняет все файлы из всех чатов/диалогов в зависимости от флага и типа медии
         /// </summary>
         /// <param name="path">Путь для сохранения фотографий</param>
         /// <param name="fromPeer">Флаг</param>
@@ -15,9 +15,9 @@
         Task ArchiveAsync(string path, FromPeer fromPeer = FromPeer.All, FromMedia fromMedia = FromMedia.Photo);
 
         /// <summary>
-        /// Возвращает фотографию из чата/диалога по названию
+        /// Сохраняет все файлы в зависимости от флага и типа меди по названию чата/диалога
         /// </summary>
-        /// <param name="path">Путь для сохранения фотографий</param>
+        /// <param name="path">Путь для сохранения файлов</param>
         /// <param name="name">Название чата/диалога</param>
         /// <param name="fromMedia">Тип медиа</param>
         Task ArchiveAsync(string path, string name, FromMedia fromMedia = FromMedia.Photo);
