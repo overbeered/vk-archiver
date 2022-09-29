80b16097cdfd8cf45eed166ea9a01a9d1d16d317
Gennady Lyashenko <gena_l_1999@mail.ru>
2022-09-29 18:13:27 +0300

    [2] add console

---
+++ samples/Console/Overbeered.VkArchiver.Console/FromMediaConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace Overbeered.VkArchiver.Console;
+
+internal class FromMediaConverter
+{
+    public static FromMedia? Converter(string media)
+    {
+        return media.ToLower() switch
+        {
+            "photo" => FromMedia.Photo,
+            "doc" => FromMedia.Doc,
+            _ => null,
+        };
+    }
+}
---
+++ samples/Console/Overbeered.VkArchiver.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Overbeered.VkArchiver.Console;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
+                .AddConsole();
+        });
+        var logVk = loggerFactory.CreateLogger<VkArchiver>();
+        var logProgram = loggerFactory.CreateLogger<Program>();
+        string login;
+        string password;
+        string name;
+        FromMedia? media;
+
+        logProgram.LogInformation("Введите login:");
+        login = System.Console.ReadLine()!;
+        logProgram.LogInformation("Введите password:");
+        password = System.Console.ReadLine()!;
+        logProgram.LogInformation("Введите name:");
+        name = System.Console.ReadLine()!;
+        logProgram.LogInformation("Введите mediaType (photo или doc):");
+        media = FromMediaConverter.Converter(System.Console.ReadLine()!)!;
+
+        var vk = new VkArchiver(logVk).CreateBuilder().SetLogin(login).SetPassword(password).SetApplicationId(8206863).Build();
+        await vk.AuthorizeAsync();
+        await vk.ArchiveAsync(@"D:\test", name, media.Value);
+        await vk.LogOutAsync();
+    }
+}
